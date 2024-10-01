5a5fa10defc6f3544192ae15899f87a3018ade42
Mhaus333 <mhouse@my.dom.edu>
2024-10-01 00:29:11 -0500

    Add files via upload

---
+++ Donor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Patient
+{
+    public class Donor
+    {
+        public Donor()
+        { }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string BloodType { get; set; }
+        public string Address { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string GetAge()
+        { return getAge(); }
+
+        private string getAge()
+        {
+            if (Birthday == DateTime.MinValue)
+            {
+                return "Unknown";
+            }
+            return (DateTime.Now.Year - Birthday.Year).ToString();
+        }
+    }
+}
+
